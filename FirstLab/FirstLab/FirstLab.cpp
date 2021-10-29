aad0bbdfdecdd80f36742ffde1ac34053e831ccc
AnotherDev <odkodksd@test.gg>
2021-10-29 20:42:20 +0300

    Initial commit. Adding first project

---
+++ FirstLab/FirstLab/FirstLab.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	string a;
+	cout << "Hello, wanderer. What's your name?\n";
+	cin >> a;
+	cout << "\nNice to meet you " << a;
+	return 0;
+}
